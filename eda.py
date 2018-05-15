a2b9093a31b61c0818094c1f496bb0fb6037eb4a
Kamil <36967931+trcz@users.noreply.github.com>
2018-05-15 11:21:19 +0200

    Update eda.py

--- eda.py
+++ eda.py
@@ -9,5 +9,19 @@ with open("train.csv", "rb") as myFile:
 print(df.info())
 print(df.nunique())
 plt.figure(figsize=(9, 8))
+
+#creating Purchase variable distribution plot and saving it as 1.png
 sns.distplot(df["Purchase"], color="b")
 plt.savefig("1.png")
+
+#dropping unnecessary columns from dataframe
+df2 = df.drop(columns=["User_ID", "Product_ID", "Product_Category_2", "Product_Category_3", "Purchase"])
+
+fig, axes = plt.subplots(round(len(df2.columns) / 3), 3, figsize=(18, 12))
+
+#creating countplots for categorical variables and saving it as 2.png
+for i, ax in enumerate(fig.axes):
+    if i < len(df2.columns):
+        ax.set_xticklabels(ax.xaxis.get_majorticklabels(), rotation=45)
+        sns.countplot(x=df2.columns[i], alpha=0.7, data=df2, ax=ax, order=sorted(df2[df2.columns[i]].unique()))
+plt.savefig("2.png")
