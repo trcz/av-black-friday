7acc993fe5dba82e4c0d3ff392775af1577df397
Kamil <36967931+trcz@users.noreply.github.com>
2018-06-05 23:32:50 +0200

    Update eda.py

--- eda.py
+++ eda.py
@@ -25,3 +25,14 @@ for i, ax in enumerate(fig.axes):
         ax.set_xticklabels(ax.xaxis.get_majorticklabels(), rotation=45)
         sns.countplot(x=df2.columns[i], alpha=0.7, data=df2, ax=ax, order=sorted(df2[df2.columns[i]].unique()))
 plt.savefig("2.png")
+
+plotnames = df.columns.values.tolist()
+toDrop = ["User_ID", "Product_ID", "Purchase"]
+plotnames = [x for x in plotnames if x not in toDrop]
+
+for i in plotnames:
+    plt.figure(figsize=(10, 6))
+    ax = sns.boxplot(x=i, y='Purchase', data=df, order=sorted(df[i].unique()))
+    plt.setp(ax.artists, alpha=.5, linewidth=2, edgecolor="k")
+    plt.xticks(rotation=45)
+    plt.savefig(i+".png")
